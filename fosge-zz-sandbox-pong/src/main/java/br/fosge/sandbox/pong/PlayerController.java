8110e713cd6174d467174e5f1d27d54f601916aa
Bruno da Silva Perez Araujo <bkraujo@outlook.com>
2025-02-05 10:42:54 -0300

    ...

--- fosge-zz-sandbox-pong/src/main/java/br/fosge/sandbox/pong/PlayerController.java
+++ fosge-zz-sandbox-pong/src/main/java/br/fosge/sandbox/pong/PlayerController.java
@@ -11,12 +11,12 @@ public final class PlayerController extends BehaviourComponent {
     public void onUpdate(double delta) {
         if (Input.active(Keyboard.KEY_D)) {
             transform.position.x += (float) (speed * delta);
-            if (transform.position.x >= 0.35f) { transform.position.x = -0.35f; }
+            if (transform.position.x >= 0.30f) { transform.position.x = -0.30f; }
         }
 
         if (Input.active(Keyboard.KEY_A)) {
             transform.position.x -= (float) (speed * delta);
-            if (transform.position.x < -0.35f) { transform.position.x = 0.35f; }
+            if (transform.position.x < -0.30f) { transform.position.x = 0.30f; }
         }
     }
 }
